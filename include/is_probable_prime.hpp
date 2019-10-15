c92409279615b6d1b5eb2790611f232dd689ef93
Joseph Noir <joseph.noir@posteo.net>
2019-10-15 16:20:20 +0200

    Fix prime check with new boost version

--- include/is_probable_prime.hpp
+++ include/is_probable_prime.hpp
@@ -9,8 +9,9 @@ CAF_POP_WARNINGS
 /// Returns `true` if `x` is probably prime and `false` if
 /// it is definitely composite. The probability to declare a
 /// composite n as probably prime is at most 0.25^trials.
-template <class T>
-bool is_probable_prime(const T& x, unsigned trials = 25) {
-  // increase 25 to a higher value for higher accuracy
-  return boost::multiprecision::miller_rabin_test(x, trials);
+inline bool is_probable_prime(const boost::multiprecision::int512_t& x,
+                              unsigned trials = 25) {
+  using namespace boost::multiprecision;
+  // Increase 25 to a higher value for higher accuracy.
+  return miller_rabin_test(static_cast<uint512_t>(x), trials);
 }
