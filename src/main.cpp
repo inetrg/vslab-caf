135b351e1ba011725840fdbafe208830692a9060
Joseph Noir <joseph.noir@posteo.net>
2019-10-15 21:26:27 +0200

    Update scaffold in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 // C++ standard library includes
-#include <string>
-#include <vector>
-#include <sstream>
-#include <iostream>
 #include <algorithm>
+#include <cstdio>
+#include <ctime>
+#include <iostream>
+#include <random>
+#include <sstream>
+#include <string>
 #include <unordered_map>
+#include <vector>
 
 // CAF includes
 #include "caf/all.hpp"
@@ -13,19 +16,21 @@
 // Boost includes
 CAF_PUSH_WARNINGS
 #include <boost/multiprecision/cpp_int.hpp>
+#include <boost/random.hpp>
 CAF_POP_WARNINGS
 
 // Own includes
-#include "is_probable_prime.hpp"
 #include "int512_serialization.hpp"
+#include "is_probable_prime.hpp"
 
-using std::cout;
 using std::cerr;
+using std::cout;
 using std::endl;
-using std::vector;
 using std::string;
 using std::unordered_map;
+using std::vector;
 
+using boost::multiprecision::gcd;
 using boost::multiprecision::int512_t;
 
 using namespace caf;
@@ -41,32 +46,80 @@ struct config : actor_system_config {
     add_message_type<int512_t>("int512_t");
     add_message_type<vector<int512_t>>("vector<int512_t>");
     opt_group{custom_options_, "global"}
-    .add(host, "host,H", "server host (ignored in server mode)")
-    .add(port, "port,p", "port")
-    .add(num_workers, "num-workers,w", "number of workers (in manager mode)")
-    .add(mode, "mode,m", "one of 'server', 'manager' or 'client'");
+      .add(host, "host,H", "server host (ignored in server mode)")
+      .add(port, "port,p", "port")
+      .add(num_workers, "num-workers,w", "number of workers (in worker mode)")
+      .add(mode, "mode,m", "one of 'server', 'worker' or 'client'");
   }
 };
 
-void run_server(actor_system& sys, const config& cfg) {
-  cout << "run_server: implement me" << endl;
+// Use this if you want to print numbers with aout.
+std::string to_string(int512_t x) {
+  std::stringstream s;
+  s << x;
+  return s.str();
 }
 
-void run_manager(actor_system& sys, const config& cfg) {
-  cout << "run_manager: implement me" << endl;
+// -- SERVER -------------------------------------------------------------------
+
+void run_server(actor_system& sys, const config& cfg) {
+  if (auto port = sys.middleman().publish_local_groups(cfg.port))
+    cout << "published local groups at port " << *port << '\n';
+  else
+    cerr << "error: " << sys.render(port.error()) << '\n';
+  cout << "press any key to exit" << std::endl;
+  getc(stdin);
 }
+
+// -- CLIENT -------------------------------------------------------------------
 
 void run_client(actor_system& sys, const config& cfg) {
-  cout << "run_client: implement me" << endl;
+  if (auto eg = sys.middleman().remote_group("vslab", cfg.host, cfg.port)) {
+    auto grp = *eg;
+    // TODO: Implement client.
+  } else {
+    cerr << "error: " << sys.render(eg.error()) << '\n';
+  }
 }
 
+// -- WORKER -------------------------------------------------------------------
+
+// State specific to each worker.
+struct worker_state {
+  // The joined group.
+  group grp;
+};
+
+behavior worker(stateful_actor<worker_state>* self, caf::group grp) {
+  // Join group and save it to send messages later.
+  self->join(grp);
+  self->state.grp = grp;
+  // TODO: Implement me.
+  // - Calculate rho.
+  // - Check for new messages in between.
+  // return { ... };
+}
+
+void run_worker(actor_system& sys, const config& cfg) {
+  if (auto eg = sys.middleman().remote_group("vslab", cfg.host, cfg.port)) {
+    auto grp = *eg;
+    // TODO: Spawn workers, e.g:
+    // sys.spawn(worker, grp);
+  } else {
+    cerr << "error: " << sys.render(eg.error()) << '\n';
+  }
+  sys.await_all_actors_done();
+}
+
+// -- MAIN ---------------------------------------------------------------------
+
 // dispatches to run_* function depending on selected mode
-void caf_main(actor_system &sys, const config &cfg) {
-  using map_t = unordered_map<string, void (*)(actor_system &, const config &)>;
+void caf_main(actor_system& sys, const config& cfg) {
+  using map_t = unordered_map<string, void (*)(actor_system&, const config&)>;
   map_t modes{
-      {"server", run_server},
-      {"manager", run_manager},
-      {"client", run_client}
+    {"server", run_server},
+    {"worker", run_worker},
+    {"client", run_client},
   };
   auto i = modes.find(cfg.mode);
   if (i != modes.end())
@@ -75,6 +128,6 @@ void caf_main(actor_system &sys, const config &cfg) {
     cerr << "*** invalid mode specified" << endl;
 }
 
-} // namespace <anonymous>
+} // namespace
 
 CAF_MAIN(io::middleman)
