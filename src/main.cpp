24a20fa09427f06ee5c2341c8923209d37186f41
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2019-10-16 14:57:15 +0200

    Fix warning unknown pragma warning on Clang

--- src/main.cpp
+++ src/main.cpp
@@ -15,9 +15,7 @@
 
 // Boost includes
 CAF_PUSH_WARNINGS
-#ifdef CAF_CLANG
-#pragma clang diagnostic ignored "-Wdeprecated-copy"
-#elif defined(CAF_GCC)
+#ifdef CAF_GCC
 #pragma GCC diagnostic ignored "-Wdeprecated-copy"
 #endif
 #include <boost/multiprecision/cpp_int.hpp>
